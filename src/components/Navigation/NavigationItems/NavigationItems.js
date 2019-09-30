a32408f08701338d434c9e88e05c4fe90ca0702d
Jared Avila <jared.c.avila@gmail.com>
2019-09-30 12:54:40 -0700

    working on navigation

---
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import * as styles from "./NavigationItems.module.css";
+import NavigationItem from "./NavigationItem/NavigationItem";
+
+const NavigationItems = props => {
+  return (
+    <ul className={styles.NavigationItems}>
+      <NavigationItem link="/" active>
+        Pizza Builder
+      </NavigationItem>
+      <NavigationItem link="/">Checkout</NavigationItem>
+    </ul>
+  );
+};
+
+export default NavigationItems;
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -3,13 +3,16 @@ import React from "react";
 import * as styles from "./Toolbar.module.css";
 
 import Logo from "../../Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems";
 
 const Toolbar = props => {
   return (
     <header className={styles.Toolbar}>
       <div>MENU</div>
       <Logo />
-      <nav>...</nav>
+      <nav>
+        <NavigationItems />
+      </nav>
     </header>
   );
 };
