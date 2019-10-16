a653ff5a45a4378a14767ab548fd5f8024130b61
Jared Avila <jared.c.avila@gmail.com>
2019-10-16 11:04:46 -0700

    styling landing

--- src/containers/Landing/Landing.js
+++ src/containers/Landing/Landing.js
@@ -1,18 +1,24 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import * as styles from "./Landing.module.css";
 
 import CityLine from "../../assets/FrontPage/skyline.png";
+import Pizza from "../../assets/FrontPage/Pizza.png";
 
 const Landing = () => {
   return (
     <div className={styles.Landing}>
       <div className={styles.Container}>
         <h1 className={styles.CityLineTitle}>CityLine Pizza</h1>
+        <NavLink to="/builder" className={styles.OrderBtn}>
+          ORDER NOW
+        </NavLink>
         <img
           className={styles.CityLineImg}
           src={CityLine}
           alt="CityLine Pizza"
         />
+        <img src={Pizza} className={styles.Pizza} alt="Pizza Logo" />
       </div>
     </div>
   );
